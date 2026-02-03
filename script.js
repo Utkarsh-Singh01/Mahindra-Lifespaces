c904b65c0310d71b10929b3abf9864a8446a1d5e
Utkarsh Singh <us6608818@gmail.com>
2026-02-03 23:19:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function openEnquiry() {
+  alert("Thanks for your interest! Our team will contact you shortly.");
+}
+function toggleMenu() {
+  const menu = document.getElementById("mobileMenu");
+  menu.classList.toggle("hidden");
+}
+
+function openEnquiry() {
+  alert("Thanks for your interest! Our team will contact you shortly.");
+}
+function goBack() {
+  window.history.back();
+}
